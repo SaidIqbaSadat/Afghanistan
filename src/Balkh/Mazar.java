f00a31de27321c9c102ecade91a0d20a92e15270
Said <iqbalsadat0@gmail.com>
2022-11-09 13:06:38 -0800

    this for update

--- src/Balkh/Mazar.java
+++ src/Balkh/Mazar.java
@@ -3,9 +3,18 @@ package Balkh;
 public class Mazar {
 
 	public static void main(String[] args) {
-	System.out.println("I love Afghanistan");
-	
+		System.out.println("I love Afghanistan");
+
 	}// TODO Auto-generated method stub
+
+	public void gitTest21() {
+		System.out.println("this is from afghan, Balkh, Mazar i shaarif city");
+	}
+
+	public void gitTest211() {
+		System.out.println("this is from afghan, Balkh, Mazar i shaarif city");
+	}
+
 	public void gitTest2() {
 		System.out.println("this is from afghan, Balkh, Mazar i shaarif city");
 	}
