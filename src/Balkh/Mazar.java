5152ca5b46f805e9fa5bde1ce2644c21b2a706fc
Said <iqbalsadat0@gmail.com>
2022-11-09 11:40:16 -0800

    this commit is 4

--- src/Balkh/Mazar.java
+++ src/Balkh/Mazar.java
@@ -3,8 +3,11 @@ package Balkh;
 public class Mazar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("I love Afghanistan");
+	
+	}// TODO Auto-generated method stub
+	public void gitTest() {
+		System.out.println("this is from afghan");
 	}
 
 }
